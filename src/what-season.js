7a3a80bc2e84a5753dedda674c8c8a4bf575963f
ANDREYKOMOV <komov.te@gmail.com>
2020-10-27 18:01:52 +0300

    feat: fix seasons

--- src/what-season.js
+++ src/what-season.js
@@ -7,23 +7,34 @@ module.exports = function getSeason(date) {
   }
 
   if (date === null) {
-    throw new Error ("Oooops");
+    throw new Error("Oooops");
   };
-  
+
+  /* if (object.prototype.toString.call(date) !== "[object Date]") {
+    throw new Error;
+  } */
+
   let er = new Date(date);
   if (!!er.getDate() === true && date instanceof Date) {
     let month = date.getMonth();
+    try {
+      date.setDate(40)
+    } catch {
+      throw new Error("Oopss");
+    }
+    
     if (month <= 1 || month === 11) {
       return "winter";
     } else if (month >= 2 && month <= 4) {
       return "spring";
     } else if (month >= 5 && month <= 7) {
       return "summer";
-    } else if (month >= 8 && month <= 10) {
+    } else if
+      (month >= 8 && month <= 10) {
       return "autumn";
     }
   } else {
-    // console.log('11111', date);
+
     throw new Error("Oopss");
   }
 };
