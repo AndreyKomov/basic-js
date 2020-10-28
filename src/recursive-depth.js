caab6217fb9bb9632b7ea07cdc266dec0aa475c1
ANDREYKOMOV <komov.te@gmail.com>
2020-10-28 13:56:27 +0300

    feat: recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,14 @@
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    
+    if (!Array.isArray(arr)) {
+      return 0;
+    };
+
+    let count = 1;
+    for (let i = 0; i < arr.length; i++) {
+      count = count + this.calculateDepth(arr[i], count)
+    }
+    return count;
   }
 };
