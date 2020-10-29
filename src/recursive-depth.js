e9698656143e940226ef31e0d6da12facd18f116
ANDREYKOMOV <komov.te@gmail.com>
2020-10-29 10:17:29 +0300

    feat: fix recursion

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,32 @@
 
 module.exports = class DepthCalculator {
+  let maxDeep = 0;
+  let count = 1;
   calculateDepth(arr) {
+
+  function calculateDepth(arr) {
     if (!Array.isArray(arr)) {
       return 0;
     };
 
-    let count = 1;
-    for (let i = 0; i < arr.length; i++) {
-      count = count + this.calculateDepth(arr[i], count)
+    if (arr.length === 0) {
+      return 1;
     }
-    return count;
+
+
+    for (let i = 0; i < arr.length; ++i) {
+
+      if (Array.isArray(arr[i])) {
+        count = 1 + this.calculateDepth(arr[i]);
+      }
+    }
+
+    if (count > maxDeep) {
+      maxDeep = count;
+      count = 1;
+    }
+
+    return maxDeep;
+  };
   }
 };
